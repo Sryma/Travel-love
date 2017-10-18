d5c3d66c1b0ee30255c5997c132bb8639b323bce
Sryma <rurahgst@gmail.com>
2017-10-18 00:09:34 -0500

    fi

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$('.your-class').slick({
+    
+    // autoplay: true,
+    arrows: false,
+    dots:true,
+    infinite:true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+     variableWidth: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    draggable: false,
+    
+  });
+});
